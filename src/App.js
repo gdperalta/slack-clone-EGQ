9fac4203e743179857b5650d2d090a00b196fd53
Godfrey Peralta <godfreydjperalta@gmail.com>
2022-02-02 22:02:17 +0800

    add sessionStorage for headers Add function to mock login Add condition to load layout or login depending on if user is logged in

--- src/App.js
+++ src/App.js
@@ -1,18 +1,33 @@
 import "./assets/styles/css/App.css";
-import { logIn, fetchUsers } from "./Utils/api";
+import { logIn, fetchUsers, register } from "./Utils/api";
 import { useEffect, useState } from "react";
 import { getHeaders } from "./Utils/getHeaders";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Users from "./Components/Users/Users";
 import Message from "./Components/Messages/Message";
-import { Layout } from "./Pages/Layout";
+import Layout from "./Pages/Layout";
+import Login from "./Components/Login/Login";
+
+/* import { io } from "socket.io-client";
+const socket = io("http://localhost:3001");
+socket.on("connect", () => {
+  console.log(`You connected with ${socket.id}`);
+}); */
 
 const App = () => {
   const [headerList, setHeaderList] = useState(null);
   const [users, setUsers] = useState(null);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   useEffect(() => {
-    logInUser();
+    const oldHeader = JSON.parse(sessionStorage.getItem("header"));
+
+    if (oldHeader) {
+      setIsLoggedIn(true);
+      setHeaderList(oldHeader);
+    } else {
+      setIsLoggedIn(false);
+    }
   }, []);
 
   useEffect(() => {
@@ -25,6 +40,7 @@ const App = () => {
     const userData = await logIn();
     const userHeader = getHeaders(userData);
     setHeaderList(userHeader);
+    sessionStorage.setItem("header", JSON.stringify(userHeader));
   };
 
   const getUsers = async () => {
@@ -32,11 +48,19 @@ const App = () => {
     setUsers(data.data);
   };
 
+  const handleLogin = () => {
+    logInUser();
+    setIsLoggedIn(true);
+  };
+
   return (
     <div>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Layout />}>
+          <Route
+            path="/"
+            element={isLoggedIn ? <Layout /> : <Login onclick={handleLogin} />}
+          >
             <Route
               index
               element={
@@ -64,4 +88,5 @@ const App = () => {
     </div>
   );
 };
+
 export default App;
