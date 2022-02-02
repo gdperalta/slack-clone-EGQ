832dca6579c2e399f4b94bef6733f2ceb1f0e8a0
Godfrey Peralta <godfreydjperalta@gmail.com>
2022-02-02 13:28:32 +0800

    Add input and useSearchParam to filter Add isloading state

--- src/Components/Users/Users.js
+++ src/Components/Users/Users.js
@@ -1,10 +1,13 @@
 import { useContext, useEffect, useState } from "react";
 import { fetchUsers } from "../../Utils/api";
 import { UserContext } from "../../Contexts/context";
+import { NavLink, useSearchParams } from "react-router-dom";
 
 const Users = () => {
   const [users, setUsers] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
   const { headerList } = useContext(UserContext);
+  let [searchParams, setSearchParams] = useSearchParams();
 
   useEffect(() => {
     if (headerList) {
@@ -13,14 +16,38 @@ const Users = () => {
   }, [headerList]);
 
   const getUsers = async () => {
+    setIsLoading(true);
     const data = await fetchUsers(headerList);
-    setUsers(data);
-    console.log(data);
+    setUsers(data.data);
+    console.log(data.data);
+    setIsLoading(false);
   };
+
+  if (isLoading) {
+    return <div>...Loading</div>;
+  }
 
   return (
     <div>
       <p>Users</p>
+      <nav>
+        <input
+          value={searchParams.get("filter") || ""}
+          onChange={(event) => {
+            let filter = event.target.value;
+            if (filter) {
+              setSearchParams({ filter });
+            } else {
+              setSearchParams({});
+            }
+          }}
+        />
+        {users.map((user) => {
+          <NavLink to={`/users/${user.id}`} key={user.id}>
+            {user.uid}
+          </NavLink>;
+        })}
+      </nav>
     </div>
   );
 };
