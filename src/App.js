2e5d90ac871a89efc3fd2c9f6b29b690317d4bfd
Godfrey Peralta <godfreydjperalta@gmail.com>
2022-02-02 15:05:41 +0800

    Refactored app.js and index.js Moved browserrouter from index to app Changed home element to Layout Removed useContext usage Declared states headerList and users in App component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
-import { Outlet } from "react-router-dom";
-import Sidebar from "./Components/Sidebar/Sidebar";
 import "./assets/styles/css/App.css";
-import { logIn } from "./Utils/api";
-import { useContext, useEffect } from "react";
+import { logIn, fetchUsers } from "./Utils/api";
+import { useEffect, useState } from "react";
 import { getHeaders } from "./Utils/getHeaders";
-import { UserContext } from "./Contexts/context";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Users from "./Components/Users/Users";
+import Message from "./Components/Messages/Message";
+import { Layout } from "./Pages/Layout";
 
 const App = () => {
-  const { setHeaderList } = useContext(UserContext);
+  const [headerList, setHeaderList] = useState(null);
+  const [users, setUsers] = useState(null);
 
   useEffect(() => {
     logInUser();
   }, []);
+
+  useEffect(() => {
+    if (headerList) {
+      getUsers();
+    }
+  }, [headerList]);
 
   const logInUser = async () => {
     const userData = await logIn();
@@ -19,12 +27,41 @@ const App = () => {
     setHeaderList(userHeader);
   };
 
+  const getUsers = async () => {
+    const data = await fetchUsers(headerList);
+    setUsers(data.data);
+  };
+
   return (
-    <div className="wrapper">
-      <Sidebar />
-      <Outlet />
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route
+              index
+              element={
+                <main style={{ padding: "1rem" }}>
+                  <p>Select a Channel</p>
+                </main>
+              }
+            />
+            <Route path="users" element={<Users users={users} />} />
+            <Route
+              path=":uid"
+              element={<Message users={users} headerList={headerList} />}
+            />
+            <Route
+              path="*"
+              element={
+                <main style={{ padding: "1rem" }}>
+                  <p>There's nothing here!</p>
+                </main>
+              }
+            />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 };
-
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,40 +1,13 @@
 import React from "react";
-import { render } from "react-dom";
+import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Users from "./Components/Users/Users";
-import Message from "./Components/Messages/Message";
-import { UserProvider } from "./Contexts/context";
 
-render(
-  <UserProvider>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />}>
-          <Route
-            index
-            element={
-              <main style={{ padding: "1rem" }}>
-                <p>Select a Channel</p>
-              </main>
-            }
-          />
-          <Route path="users" element={<Users />} />
-          <Route path=":uid" element={<Message />} />
-          <Route
-            path="*"
-            element={
-              <main style={{ padding: "1rem" }}>
-                <p>There's nothing here!</p>
-              </main>
-            }
-          />
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  </UserProvider>,
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
   document.getElementById("root")
 );
 
