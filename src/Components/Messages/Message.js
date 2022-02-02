080a7cdbfb92ce451f647188a2094a34b870fac6
Godfrey Peralta <godfreydjperalta@gmail.com>
2022-02-02 15:08:04 +0800

    Add function to get selected user details

--- src/Components/Messages/Message.js
+++ src/Components/Messages/Message.js
@@ -1,12 +1,17 @@
-export const testData = {
-  name: "Godfrey",
-  uid: 1,
-};
+import { useParams } from "react-router-dom";
 
-const Message = () => {
+const Message = ({ users }) => {
+  let params = useParams();
+
+  const getUser = (id) => {
+    return users.find((user) => user.id === id);
+  };
+
+  let user = getUser(parseInt(params.uid));
+
   return (
     <div>
-      <p>{testData.name}</p>
+      <p>{user.uid}</p>
     </div>
   );
 };
