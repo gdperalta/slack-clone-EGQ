3bf6c22ad6550e2241b9da1327ecd03dbac6215d
ermzkie <ermawindelynlagustan@gmail.com>
2022-02-03 21:06:24 +0800

    Added modal for creating channels

--- src/Components/Channels/AddNewChannel.js
+++ src/Components/Channels/AddNewChannel.js
@@ -1,22 +1,35 @@
 import { useState } from "react";
+import React from "react";
+import { useNavigate } from "react-router-dom";
 import { createChannel } from "../../Utils/channelAPI";
 import { logIn } from "../../Utils/api";
 import { getHeaders } from "../../Utils/getHeaders";
+import ChannelModal from "./ChannelModal";
 
 const AddNewChannel = () => {
+  let navigate = useNavigate();
   const [channelName, setChannelName] = useState("");
+  const [filterEmail, setFilterEmail] = useState("");
+
+  const [show, setShow] = useState(true);
 
   const handleChange = (event) => {
     event.preventDefault();
-    setChannelName(event.target.value);
+
+    if (event.target.name == "channelName") {
+      setChannelName(event.target.value);
+    } else if (event.target.name == "filterEmailAdd") {
+      setFilterEmail(event.target.value);
+    }
   };
 
-  const showChannelName = async (event) => {
+  const createChannelName = async (event) => {
     event.preventDefault();
 
     const response = await logIn();
-    const header = getHeaders(response);
+    console.log(response);
 
+    const header = getHeaders(response);
     const result = await createChannel(channelName, header);
 
     console.log(result);
@@ -24,16 +37,39 @@ const AddNewChannel = () => {
 
   return (
     <div>
-      <form onSubmit={showChannelName}>
-        <label>Channel Name: </label>
-        <input
-          type="text"
-          name="channelName"
-          value={channelName}
-          onChange={handleChange}
-        />
-        <input value="Next" type="submit" />
-      </form>
+      <ChannelModal
+        title="My modal"
+        onClose={() => {
+          setShow(false);
+          navigate("/");
+        }}
+        show={show}
+      >
+        <form onSubmit={createChannelName}>
+          <label>Channel Name: </label>
+          <input
+            type="text"
+            name="channelName"
+            value={channelName}
+            onChange={handleChange}
+          />
+          <div>
+            <div>
+              <h4>Add members</h4>
+              <input
+                type="search"
+                name="filterEmailAdd"
+                placeholder="Search by name or email address"
+                value={filterEmail}
+                onChange={handleChange}
+              />
+            </div>
+            <div>
+              <input value="Save" type="submit" />
+            </div>
+          </div>
+        </form>
+      </ChannelModal>
     </div>
   );
 };
