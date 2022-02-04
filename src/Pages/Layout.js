8c7a4d2139879014f41403b4e8f6ba0335630687
Godfrey Peralta <godfreydjperalta@gmail.com>
2022-02-04 22:56:16 +0800

    Add header component

--- src/Pages/Layout.js
+++ src/Pages/Layout.js
@@ -1,10 +1,12 @@
 import { Outlet, Link } from "react-router-dom";
 import Channels from "../Components/Channels/Channels";
+import Header from "../Components/Header/Header";
 import DirectMessages from "../Components/Messages/DirectMessages";
 
 const Layout = ({ headerList, changeReceiver, receiverEmail, messageSent }) => {
   return (
     <div className="wrapper">
+      <Header />
       <div className="sidebar-wrapper">
         <nav>
           <Link to="/users">Message A User</Link>
