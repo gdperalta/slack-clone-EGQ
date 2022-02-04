63fdc138f45078ff010f2f8bc4b83b7a226d2b31
Godfrey Peralta <godfreydjperalta@gmail.com>
2022-02-04 20:24:08 +0800

    Add messageSent state in useEffect watcher Changed Messages element from p to h3 removed style in NavLink and added classname instead

--- src/Components/Messages/DirectMessages.js
+++ src/Components/Messages/DirectMessages.js
@@ -1,9 +1,14 @@
 import { useEffect, useState } from "react";
 import { NavLink } from "react-router-dom";
 import { fetchRecentMsgs } from "../../Utils/api";
-import { createUniqueArray } from "../../Utils/createUniqueArray";
+import { createUniqueArray } from "../../Utils/handleArrays";
 
-const DirectMessages = ({ headerList, changeReceiver }) => {
+const DirectMessages = ({
+  headerList,
+  changeReceiver,
+  receiverEmail,
+  messageSent,
+}) => {
   const [recentMessages, setRecentMessages] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
 
@@ -12,9 +17,15 @@ const DirectMessages = ({ headerList, changeReceiver }) => {
     if (headerList) {
       getDirectMessages();
     }
-  }, []);
+  }, [messageSent]);
 
   const getDirectMessages = async () => {
+    /* if (recentMessages) {
+      const i = recentMessages.findIndex((user) => {
+        return user.email === receiverEmail;
+      });
+    } */
+
     const recentDMs = await fetchRecentMsgs(headerList);
     const uniqueUsers = createUniqueArray(recentDMs.data);
 
@@ -28,18 +39,14 @@ const DirectMessages = ({ headerList, changeReceiver }) => {
 
   return (
     <div>
-      <p>Messages</p>
+      <h3>Messages</h3>
       <nav>
         {recentMessages.map((user) => {
           return (
             <NavLink
-              style={({ isActive }) => {
-                return {
-                  display: "block",
-                  margin: "1rem 0",
-                  color: isActive ? "green" : "",
-                };
-              }}
+              className={({ isActive }) =>
+                isActive ? "recentMessages activeMsg" : "recentMessages"
+              }
               to={`/${user.id}`}
               key={user.id}
               onClick={changeReceiver}
