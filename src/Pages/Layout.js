921babf614896fb72b0b960e8b5da471fcfd20a2
Godfrey Peralta <godfreydjperalta@gmail.com>
2022-02-04 20:28:51 +0800

    Add props receiverEmail and messageSent

--- src/Pages/Layout.js
+++ src/Pages/Layout.js
@@ -2,16 +2,18 @@ import { Outlet, Link } from "react-router-dom";
 import Channels from "../Components/Channels/Channels";
 import DirectMessages from "../Components/Messages/DirectMessages";
 
-const Layout = ({ headerList, changeReceiver }) => {
+const Layout = ({ headerList, changeReceiver, receiverEmail, messageSent }) => {
   return (
     <div className="wrapper">
       <div className="sidebar-wrapper">
         <nav>
-          <Link to="/users">Users</Link>
+          <Link to="/users">Message A User</Link>
           <Channels />
           <DirectMessages
             changeReceiver={changeReceiver}
             headerList={headerList}
+            receiverEmail={receiverEmail}
+            messageSent={messageSent}
           />
         </nav>
       </div>
