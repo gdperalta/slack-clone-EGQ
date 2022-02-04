d9e28066ebaa6def79f81eed25b1d898af652626
Godfrey Peralta <godfreydjperalta@gmail.com>
2022-02-04 22:29:31 +0800

    Add icon in messageHeader

--- src/Components/Messages/MessageHeader.js
+++ src/Components/Messages/MessageHeader.js
@@ -1,6 +1,7 @@
 const MessageHeader = ({ receiver }) => {
   return (
     <div className="messageHeader">
+      <span className="icon">{receiver.uid.charAt(0).toUpperCase()}</span>
       <h3>{receiver.uid}</h3>
     </div>
   );
