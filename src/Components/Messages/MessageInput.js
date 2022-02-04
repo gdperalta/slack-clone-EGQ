cd7cae4b2a8fc6e911954bfd99ecfe9ddc8dee19
Godfrey Peralta <godfreydjperalta@gmail.com>
2022-02-04 20:31:11 +0800

    Add sendButton for button icon Add placeholder for input Add classname to div named inputWrapper

--- src/Components/Messages/MessageInput.js
+++ src/Components/Messages/MessageInput.js
@@ -1,17 +1,22 @@
 import { useState } from "react";
+import sendButton from "../../assets/images/send.png";
 
 const MessageInput = ({ sendMessage }) => {
   const [message, setMessage] = useState("");
 
   return (
-    <div>
+    <div className="inputWrapper">
       <input
         value={message}
         onChange={(e) => {
           setMessage(e.target.value);
         }}
+        placeholder="Type a message"
       />
-      <button onClick={() => sendMessage(message)}>Send</button>
+
+      <button onClick={() => sendMessage(message)}>
+        <img src={sendButton} />
+      </button>
     </div>
   );
 };
