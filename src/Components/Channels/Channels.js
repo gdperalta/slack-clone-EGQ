f0bc77a316dd0b89e0f2056d94f7879c7dc4b062
ermzkie <ermawindelynlagustan@gmail.com>
2022-02-06 22:57:43 +0800

    Transferred channel APIs to the api file

--- src/Components/Channels/Channels.js
+++ src/Components/Channels/Channels.js
@@ -1,16 +1,14 @@
 import React, { useState, useEffect } from "react";
-import { getUserChannels } from "../../Utils/channelAPI";
+import { getUserChannels } from "../../Utils/api";
 import Channel from "./Channel";
 import AddNewChannel from "./AddNewChannel";
-
 
 const Channels = () => {
   const [userChannels, setUserChannels] = useState({});
   const [show, setShow] = useState(false);
-  
+
   useEffect(() => {
     const fetchChannels = async () => {
-  
       const header = JSON.parse(sessionStorage.getItem("header"));
       const channels = await getUserChannels(header);
       setUserChannels(channels);
@@ -28,12 +26,24 @@ const Channels = () => {
   return (
     <div>
       <p>Channels</p>
-      {userChannels.data !== undefined ? (
-        renderChannelList()
-      ) : (
-        <p>Loading channels</p>
-      )}
-      <button onClick={() => setShow(true)}>Add new channel</button>
+      <nav>
+        {userChannels.data !== undefined ? (
+          renderChannelList()
+        ) : (
+          <p>Loading channels</p>
+        )}
+      </nav>
+      <div className="channel-create-button" onClick={() => setShow(true)}>
+        <svg
+          className="svg-plus-sign"
+          focusable="false"
+          viewBox="0 0 24 24"
+          aria-hidden="true"
+        >
+          <path d="M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2z"></path>
+        </svg>
+        Add new channel
+      </div>
       <AddNewChannel
         title="Create new channel"
         onClose={() => setShow(false)}
