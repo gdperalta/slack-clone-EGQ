960778a5ffeb71944eea176b988b7283aed1017a
Godfrey Peralta <godfreydjperalta@gmail.com>
2022-02-06 21:52:21 +0800

    Removed email address from name display

--- src/Components/Messages/MessageHeader.js
+++ src/Components/Messages/MessageHeader.js
@@ -2,7 +2,7 @@ const MessageHeader = ({ receiver }) => {
   return (
     <div className="messageHeader">
       <span className="icon">{receiver.uid.charAt(0).toUpperCase()}</span>
-      <h3>{receiver.uid}</h3>
+      <h3>{receiver.uid.split("@")[0]}</h3>
     </div>
   );
 };
