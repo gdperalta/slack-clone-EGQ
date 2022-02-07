be40473855e435a0503dfc9d88e03ea80edf012a
Godfrey Peralta <godfreydjperalta@gmail.com>
2022-02-07 22:57:33 +0800

    Changed div messageBody to a Link that routes user to message the clicked user

--- src/Components/Users/UserDirectMessages.js
+++ src/Components/Users/UserDirectMessages.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import { fetchMessages, fetchRecentMsgs } from "../../Utils/api";
 import { createUniqueArray } from "../../Utils/handleArrays";
 import { getDate, getTime } from "../../Utils/handleDate";
@@ -41,35 +42,34 @@ const UserDirectMessages = ({ headerList, userDetails }) => {
           return (
             <div className="messageBodyWrapper" key={message.id}>
               {getDate(message.created_at)}
-              <div className="messageBody">
-                <div>
-                  {userDetails.email === message.sender.email ? (
-                    <div>
-                      <b className="iconUsers">
-                        {message.receiver.email.charAt(0).toUpperCase()}
-                      </b>
-                      <div className="msg">
-                        <h3>{message.receiver.email.split("@")[0]}</h3>
-                        <span>You: </span>
-                        <span>{message.body}</span>
-                      </div>
-                    </div>
-                  ) : (
-                    <div>
-                      <b className="iconUsers">
-                        {message.sender.email.charAt(0).toUpperCase()}
-                      </b>
-                      <div className="msg">
-                        <h3>{message.sender.email.split("@")[0]}</h3>
-                        <span>{`${message.sender.email.split("@")[0]}: `}</span>
-                        <span>{message.body}</span>
-                      </div>
-                    </div>
-                  )}
-                </div>
-
-                {getTime(message.created_at)}
-              </div>
+              {userDetails.email === message.sender.email ? (
+                <Link
+                  to={`/User_${message.receiver.id}`}
+                  className="messageBody"
+                >
+                  <b className="iconUsers">
+                    {message.receiver.email.charAt(0).toUpperCase()}
+                  </b>
+                  <div className="msg">
+                    <h3>{message.receiver.email.split("@")[0]}</h3>
+                    <span>You: </span>
+                    <span>{message.body}</span>
+                  </div>
+                  {getTime(message.created_at)}
+                </Link>
+              ) : (
+                <Link to={`/User_${message.sender.id}`} className="messageBody">
+                  <b className="iconUsers">
+                    {message.sender.email.charAt(0).toUpperCase()}
+                  </b>
+                  <div className="msg">
+                    <h3>{message.sender.email.split("@")[0]}</h3>
+                    <span>{`${message.sender.email.split("@")[0]}: `}</span>
+                    <span>{message.body}</span>
+                  </div>
+                  {getTime(message.created_at)}
+                </Link>
+              )}
             </div>
           );
       })}
