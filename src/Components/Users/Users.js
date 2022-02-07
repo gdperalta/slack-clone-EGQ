9d07c80662ad93aebf2ef3a2d12c77ee8168b501
Godfrey Peralta <godfreydjperalta@gmail.com>
2022-02-07 22:28:42 +0800

    Refactored Users component Moved input search bar and nav links of users to a sub component

--- src/Components/Users/Users.js
+++ src/Components/Users/Users.js
@@ -1,9 +1,13 @@
-import { useEffect, useState } from "react";
-import { NavLink, useSearchParams } from "react-router-dom";
+import { useEffect, useState, useRef } from "react";
+import { useSearchParams } from "react-router-dom";
+import SearchBar from "./SearchBar";
+import UserDirectMessages from "./UserDirectMessages";
+import UserSearchModal from "./UserSearchModal";
 
-const Users = ({ users, changeMessageDisplay }) => {
+const Users = ({ users, changeMessageDisplay, headerList, userDetails }) => {
   const [isLoading, setIsLoading] = useState(true);
   let [searchParams, setSearchParams] = useSearchParams();
+  const searchModal = useRef(null);
 
   useEffect(() => {
     setIsLoading(true);
@@ -12,52 +16,45 @@ const Users = ({ users, changeMessageDisplay }) => {
     }
   }, [users]);
 
+  const handleSearchModal = (truthy) => {
+    if (truthy) {
+      searchModal.current.classList.add("showModal");
+    } else {
+      searchModal.current.classList.remove("showModal");
+    }
+  };
+
+  const handleSearchFilter = (filter) => {
+    if (filter) {
+      setSearchParams({ filter });
+      handleSearchModal(true);
+    } else {
+      setSearchParams({});
+      handleSearchModal(false);
+    }
+  };
+
   if (isLoading) {
     return <div>...Loading</div>;
   }
 
   return (
     <div className="outletWrapper">
-      <p>Users</p>
-      <nav>
-        <input
-          value={searchParams.get("filter") || ""}
-          onChange={(event) => {
-            let filter = event.target.value;
-            if (filter) {
-              setSearchParams({ filter });
-            } else {
-              setSearchParams({});
-            }
-          }}
+      <div className="outletHeader">
+        <h3>All direct messages</h3>
+      </div>
+      <SearchBar
+        searchParams={searchParams}
+        handleSearchFilter={handleSearchFilter}
+      />
+      <div ref={searchModal} className="navlinkWrapper">
+        <UserSearchModal
+          users={users}
+          searchParams={searchParams}
+          changeMessageDisplay={changeMessageDisplay}
         />
-        {users
-          .filter((user) => {
-            let filter = searchParams.get("filter");
-            //Returned "" if search input is blank to show empty search list on initial render
-            if (!filter) return "";
-            let name = user.uid.toLowerCase();
-            return name.startsWith(filter.toLowerCase());
-          })
-          .map((user) => {
-            return (
-              <NavLink
-                style={({ isActive }) => {
-                  return {
-                    display: "block",
-                    margin: "1rem 0",
-                    color: isActive ? "green" : "",
-                  };
-                }}
-                to={`/${user.id}`}
-                key={user.id}
-                onClick={changeMessageDisplay}
-              >
-                {user.uid}
-              </NavLink>
-            );
-          })}
-      </nav>
+      </div>
+      <UserDirectMessages headerList={headerList} userDetails={userDetails} />
     </div>
   );
 };
