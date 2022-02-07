d24fe0a0eac1bb02ef393250e7e0aca4863a820b
Godfrey Peralta <godfreydjperalta@gmail.com>
2022-02-07 22:26:40 +0800

    Changed classname of messageHeader to outletHeader

--- src/Components/Messages/MessageHeader.js
+++ src/Components/Messages/MessageHeader.js
@@ -4,12 +4,12 @@ const MessageHeader = ({ receiver }) => {
   return (
     <>
       {receiver.email ? (
-        <div className="messageHeader">
+        <div className="outletHeader">
           <span className="icon">{receiver.email.charAt(0).toUpperCase()}</span>
           <h3>{receiver.email.split("@")[0]}</h3>
         </div>
       ) : (
-        <div className="messageHeader">
+        <div className="outletHeader">
           <FaLock />
           <h3>{receiver.name}</h3>
         </div>
