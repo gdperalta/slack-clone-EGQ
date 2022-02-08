e715fceff53a7223c52e89b3825e02a0fbfd51a2
Godfrey Peralta <godfreydjperalta@gmail.com>
2022-02-08 22:20:06 +0800

    Add files needed to test App Add initial tests for App component

--- src/App.test.js
+++ src/App.test.js
@@ -1,23 +1,54 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent, cleanup } from "@testing-library/react";
 import App from "./App";
 import { handlers } from "./testServer";
 import { setupServer } from "msw/node";
+import userEvent from "@testing-library/user-event";
 import { rest } from "msw";
+import { act } from "react-dom/test-utils";
 
 const server = setupServer(...handlers);
 
 beforeAll(() => {
+  //render(<App />);
+  //const loginBtn = screen.getByText("Sign in with Email");
+
   server.listen();
 });
 afterEach(() => {
+  cleanup;
   server.resetHandlers();
 });
 afterAll(() => {
   server.close();
 });
 
-test("renders learn react link", () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test("should login and display interface when logged in", async () => {
+  const { findByText } = render(<App />);
+  const loginBtn = screen.getByText("Sign In with Email");
+  userEvent.click(loginBtn);
+
+  const header = await findByText("Header");
+  expect(header).toBeInTheDocument();
+
+  const dm = await findByText("Direct Messages");
+  expect(dm).toBeInTheDocument();
+
+  const user = await findByText("odie");
+  expect(user).toBeInTheDocument();
+
+  const outlet = await findByText("Select a Channel");
+  expect(outlet).toBeInTheDocument();
+});
+
+test("should login and display interface when logged in", async () => {
+  const { findByText } = render(<App />);
+  /*const loginBtn = screen.getByText("Sign In with Email");
+  userEvent.click(loginBtn); */
+
+  const user = await findByText("odie");
+  userEvent.click(user);
+
+  const msgHeader = await findByText("Hi odie");
+
+  expect(msgHeader).toBeInTheDocument();
 });
--- src/testServer.js
+++ src/testServer.js
@@ -1,20 +1,108 @@
 import { rest } from "msw";
 
 const handlers = [
-  rest.get("http://206.189.91.54//api/v1/users", (req, res, ctx) => {
-    return res(ctx.status(200), ctx.json({}));
+  rest.post("http://206.189.91.54//api/v1/auth/sign_in", (req, res, ctx) => {
+    return res(
+      ctx.status(200),
+      ctx.json({
+        data: { email: "odie@gmail.com", id: 123 },
+      })
+    );
   }),
-  rest.get("http://206.189.91.54//api/v1/messages", (req, res, ctx) => {
+  rest.get("http://206.189.91.54//api/v1/users", (req, res, ctx) => {
+    return res(
+      ctx.status(200),
+      ctx.json({
+        data: [
+          {
+            id: 1,
+            uid: "odie@gmail.com",
+            email: "odie@gmail.com",
+          },
+          {
+            id: 2,
+            uid: "quev@gmail.com",
+            email: "quev@gmail.com",
+          },
+          {
+            id: 3,
+            uid: "erma@gmail.com",
+            email: "erma@gmail.com",
+          },
+          {
+            id: 4,
+            uid: "dio@gmail.com",
+            email: "dio@gmail.com",
+          },
+        ],
+      })
+    );
+  }),
+  rest.post("http://206.189.91.54//api/v1/messages", (req, res, ctx) => {
     return res(ctx.status(200), ctx.json({}));
   }),
   rest.get(
-    "http://206.189.91.54//api/v1/messages?receiver_class=${msgClass}&receiver_id=${id}",
+    `http://206.189.91.54//api/v1/messages?receiver_class=User&receiver_id=1`,
     (req, res, ctx) => {
-      return res(ctx.status(200), ctx.json());
+      return res(
+        ctx.status(200),
+        ctx.json({
+          data: [
+            {
+              body: "Hi",
+              created_at: "2022-02-03T12:46:48.839Z",
+              id: 14431,
+              receiver: {
+                id: 1,
+                uid: "odie@gmail.com",
+                email: "odie@gmail.com",
+              },
+              sender: {
+                id: 2,
+                uid: "quev@gmail.com",
+                email: "quev@gmail.com",
+              },
+            },
+            {
+              body: "hei",
+              created_at: "2022-02-03T12:46:48.839Z",
+              id: 14431,
+              receiver: {
+                id: 2,
+                uid: "quev@gmail.com",
+                email: "quev@gmail.com",
+              },
+              sender: {
+                id: 1,
+                uid: "odie@gmail.com",
+                email: "odie@gmail.com",
+              },
+            },
+          ],
+        })
+      );
     }
   ),
   rest.get("http://206.189.91.54//api/v1/users/recent", (req, res, ctx) => {
-    return res(ctx.status(200), ctx.json({}));
+    return res(
+      ctx.status(200),
+      ctx.json({
+        data: [
+          {
+            id: 1,
+            uid: "odie@gmail.com",
+          },
+          {
+            id: 2,
+            uid: "quev@gmail.com",
+          },
+          {
+            id: 3,
+            uid: "erma@gmail.com",
+          },
+        ],
+      })
+    );
   }),
   //prevents actual request from going out to the internet when you forget to add the
   //proper request handler
@@ -27,5 +115,17 @@ const handlers = [
     );
   }),
 ];
+
+/* const mockUserStorage = {
+  email: "odie@gmail.com",
+  id: 123,
+};
+
+const mockHeaderStorage = {
+  accessToken: "xxx",
+  client: "yyy",
+  expiry: "1645530342",
+  uid: "odie@gmail.com",
+}; */
 
 export { handlers };
