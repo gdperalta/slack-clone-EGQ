16c5e4a1107ecf954cad3ae7896e77065f536bc7
ermzkie <ermawindelynlagustan@gmail.com>
2022-02-09 23:22:22 +0800

    Added handler for getting channels

--- src/testServer.js
+++ src/testServer.js
@@ -1,4 +1,5 @@
 import { rest } from "msw";
+import { dummyChannels } from "./Utils/mockData";
 
 const handlers = [
   rest.get("http://206.189.91.54//api/v1/users", (req, res, ctx) => {
@@ -15,6 +16,26 @@ const handlers = [
   ),
   rest.get("http://206.189.91.54//api/v1/users/recent", (req, res, ctx) => {
     return res(ctx.status(200), ctx.json({}));
+  }),
+  rest.get("http://206.189.91.54//api/v1/channels", (req, res, ctx) => {
+    return res(ctx.status(200), ctx.json(dummyChannels));
+  }), 
+  rest.post("http://206.189.91.54//api/v1/auth/sign_in", (req, res, ctx) => {
+    return res(
+      ctx.status(200),
+      ctx.json({
+        data: {
+          allow_password_change: false,
+          email: "ewl@gmail.com",
+          id: 1675,
+          image: null,
+          name: null,
+          nickname: null,
+          provider: "email",
+          uid: "ewl@gmail.com",
+        },
+      })
+    );
   }),
   //prevents actual request from going out to the internet when you forget to add the
   //proper request handler
