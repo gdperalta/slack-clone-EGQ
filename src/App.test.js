b32d44ad6731c0cb0bfc472d2dba0ea6dfbd1192
ermzkie <ermawindelynlagustan@gmail.com>
2022-02-09 23:17:59 +0800

    Added test to check if channels were rendered

--- src/App.test.js
+++ src/App.test.js
@@ -1,4 +1,5 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import App from "./App";
 import { handlers } from "./testServer";
 import { setupServer } from "msw/node";
@@ -20,4 +21,22 @@ test("renders learn react link", () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+test("renders list of channels", async () => {
+ /*  server.use(
+ 
+  ); */
+
+  render(<App />);
+  const btnLogin = screen.getByText("Sign In with Email");
+  userEvent.click(btnLogin);
+
+  //expect(await screen.findByText("Channels")).toBeInTheDocument();
+  const btnChannels =  await screen.findByText("Channels");
+  userEvent.click(btnChannels);
+  screen.debug();
+
+  /*  const channel = screen.getByText("egq123-2");
+  expect(channel).toBeInTheDocument(); */
 });
