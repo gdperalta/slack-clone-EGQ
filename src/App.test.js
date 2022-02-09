a8918fd29ce1318db81a4faca9666e35809ab6c0
Godfrey Peralta <godfreydjperalta@gmail.com>
2022-02-09 18:43:05 +0800

    Update imports from test server Add testings for all message interactions with user

--- src/App.test.js
+++ src/App.test.js
@@ -1,54 +1,81 @@
-import { render, screen, fireEvent, cleanup } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import App from "./App";
-import { handlers } from "./testServer";
+import { messageHandlers } from "./testServer";
 import { setupServer } from "msw/node";
 import userEvent from "@testing-library/user-event";
-import { rest } from "msw";
-import { act } from "react-dom/test-utils";
 
-const server = setupServer(...handlers);
+const server = setupServer(...messageHandlers);
 
 beforeAll(() => {
-  //render(<App />);
-  //const loginBtn = screen.getByText("Sign in with Email");
-
   server.listen();
 });
 afterEach(() => {
-  cleanup;
   server.resetHandlers();
 });
 afterAll(() => {
   server.close();
 });
 
-test("should login and display interface when logged in", async () => {
-  const { findByText } = render(<App />);
-  const loginBtn = screen.getByText("Sign In with Email");
-  userEvent.click(loginBtn);
+describe("App Navigation and Interaction", () => {
+  test("should login and navigate to home page", async () => {
+    render(<App />);
+    const loginBtn = screen.getByText("Sign In with Email");
+    userEvent.click(loginBtn);
 
-  const header = await findByText("Header");
-  expect(header).toBeInTheDocument();
+    expect(await screen.findByText("Header")).toBeInTheDocument();
+    expect(await screen.findByText("Direct Messages")).toBeInTheDocument();
+    expect(await screen.findByText("Select a Channel")).toBeInTheDocument();
+  });
 
-  const dm = await findByText("Direct Messages");
-  expect(dm).toBeInTheDocument();
+  test("show display messages with selected user", async () => {
+    render(<App />);
 
-  const user = await findByText("odie");
-  expect(user).toBeInTheDocument();
+    const user = await screen.findByRole("link", { name: /dio/i });
+    userEvent.click(user);
 
-  const outlet = await findByText("Select a Channel");
-  expect(outlet).toBeInTheDocument();
-});
+    const msgHeader = await screen.findByRole("heading", {
+      name: /dio/i,
+      level: 2,
+    });
 
-test("show display messages with user", async () => {
-  const { findByText } = render(<App />);
-  /*const loginBtn = screen.getByText("Sign In with Email");
-  userEvent.click(loginBtn); */
+    expect(msgHeader).toBeInTheDocument();
+    expect(await screen.findByText("Hi")).toBeInTheDocument();
+    expect(await screen.findByText("Hey")).toBeInTheDocument();
+  });
 
-  const user = await findByText("odie");
-  userEvent.click(user);
+  test("sends message and shows message in message screen", async () => {
+    render(<App />);
 
-  const msgHeader = await findByText("Hi odie");
+    userEvent.click(await screen.findByTitle("Send Message"));
 
-  expect(msgHeader).toBeInTheDocument();
+    expect(await screen.findByText("Hello World")).toBeInTheDocument();
+  });
+
+  test("shows all direct messages and routes to user conversation on click of message", async () => {
+    render(<App />);
+
+    const openDMbtn = await screen.findByTitle("Open a Direct Message");
+    userEvent.click(openDMbtn);
+
+    expect(await screen.findByText("All direct messages")).toBeInTheDocument();
+
+    const searchUserBox = await screen.findByPlaceholderText(/somebody/i);
+    userEvent.type(searchUserBox, "erma");
+    expect(await screen.findByText("erma@gmail.com")).toBeInTheDocument();
+
+    const directMsgs = await screen.findAllByText("Hey");
+    for (const msg of directMsgs) {
+      expect(msg).toBeInTheDocument();
+    }
+
+    userEvent.click(directMsgs[0]);
+    expect(
+      await screen.findByRole("heading", {
+        name: /dio/i,
+        level: 2,
+      })
+    ).toBeInTheDocument();
+  });
+
+  sessionStorage.clear();
 });
